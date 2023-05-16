34fe72c97e05d72c76609d3b6b1a7da1eb52b3f8
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-16 23:15:33 +0530

    Add test cases, function is funtional for upto 23 elements

--- src/set.js
+++ src/set.js
@@ -1,15 +1,19 @@
 const getPowerSet = function(set) {
   const powerset = [[]];
- 
+  // fs.writeFileSync('./write.txt', `${powerset}\n`);
+
   set.forEach((number) => {
     powerset.forEach((subset) => {
-      const nextSubset = [...subset];
-      // powerset.push([...nextSubset, number]);
-      fs.appendFileSync('./write.txt', `${[...nextSubset, number]}`);
+      // const nextSubset = [...subset];
+      powerset.push([...[...subset], number]);
+      //  let current = [...nextSubset, number];
+      // fs.appendFileSync('./write.txt', `${powerset.toString()}\n`);
     })
   })
 
-  // return powerset;
+  return powerset;
 };
+
+console.log(getPowerSet([1,2,3]));
 
 exports.getPowerSet = getPowerSet;
--- test/set-test.js
+++ test/set-test.js
@@ -1,10 +1,18 @@
-const {describe,it} = require('node:test');
-const {deepStrictEqual} = require('assert');
+const { describe, it } = require('node:test');
+const { deepStrictEqual } = require('assert');
 
-const {getPowerSet} = require('../src/set.js');
+const { getPowerSet } = require('../src/set.js');
 
 describe('getPowerSet', () => {
   it('should give 1 empty subset for empty set', () => {
     deepStrictEqual(getPowerSet([]), [[]]);
+  });
+
+  it('should give 2 subsets for single element set', () => {
+    deepStrictEqual(getPowerSet([1]), [[], [1]]);
+  });
+
+  it('should give 4 subsets for set of 2 elements', () => {
+    deepStrictEqual(getPowerSet([1, 2]), [[], [1], [2], [1, 2]]);
   });
 });
